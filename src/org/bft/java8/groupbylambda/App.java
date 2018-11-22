a5850d38d489c237a36edd1482f5ef64e5422fc1
Benfarhat <benfarhat.elyes@gmail.com>
2018-11-22 21:05:02 +0100

    group by with lambda and Map.Entry loop

---
+++ src/org/bft/java8/groupbylambda/App.java
@@ -0,0 +1,47 @@
+package org.bft.java8.groupbylambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		List<Product> products = new ArrayList<Product>();
+		products.add(new Product("p1", "mobile 1", 1000, 2, "Manufacturer 1"));
+		products.add(new Product("p2", "mobile 2", 800, 4, "Manufacturer 2"));
+		products.add(new Product("p3", "mobile 3", 900, 5, "Manufacturer 1"));
+		products.add(new Product("p4", "laptop 1", 950, 7, "Manufacturer 2"));
+		products.add(new Product("p5", "laptop 2", 1025, 8, "Manufacturer 2"));
+		products.add(new Product("p6", "laptop 3", 1005, 9, "Manufacturer 1"));
+		
+
+		System.out.println("*************");
+		System.out.println("* Group by  *");
+		System.out.println("*************\n");
+		
+		
+		Map<String, Long> group = products.stream()
+				.collect(Collectors.groupingBy(Product::getManufacturer, Collectors.counting()));
+		
+		System.out.println(group);
+
+		System.out.println("*************");
+		System.out.println("* Details  *");
+		System.out.println("*************\n");	
+		
+		for(Entry<String, Long> entry : group.entrySet()) {
+			
+			System.out.println("key: " + entry.getKey());
+			System.out.println("value : " + entry.getValue());
+
+		}
+		
+			
+
+	}
+
+}
---
+++ src/org/bft/java8/groupbylambda/Product.java
@@ -0,0 +1,108 @@
+package org.bft.java8.groupbylambda;
+
+public class Product {
+	private String id;
+	private String name;
+	private long price;
+	private int quantity;
+	private String manufacturer;
+
+	public Product() {
+	}
+
+	/**
+	 * @param id
+	 * @param name
+	 * @param price
+	 * @param quantity
+	 * @param manufacturer
+	 */
+	public Product(String id, String name, long price, int quantity, String manufacturer) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+		this.manufacturer = manufacturer;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public long getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(long price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	/**
+	 * @return the manufacturer
+	 */
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	/**
+	 * @param manufacturer the manufacturer to set
+	 */
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", quantity=" + quantity
+				+ ", manufacturer=" + manufacturer + "]";
+	}
+	
+}
