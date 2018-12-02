b4beeece86be58734c064ad40cdc70e1b2ecab1f
Benfarhat <benfarhat.elyes@gmail.com>
2018-12-02 22:56:22 +0100

    Runtime , GC, total/max/freeMemory, availableProcessors

---
+++ src/org/bft/java/runtime/App.java
@@ -0,0 +1,39 @@
+package org.bft.java.runtime;
+
+class People {
+	String firstName, lastName;
+	int yearBirth;
+	public People(String firstName, String lastName, int yearBirth) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.yearBirth = yearBirth;
+	}
+}
+public class App {
+
+	public static void main(String[] args) {
+		Runtime r = Runtime.getRuntime(); 
+		System.out.println ("Free : " + r.freeMemory());
+		People ja = new People("John", "Doe", 1970);
+		People jb = new People("Jane", "Doe", 1980);
+		System.out.println ("Processors : " + r.availableProcessors());
+		System.out.println ("Total : " + r.totalMemory());
+		System.out.println ("Max : " + r.maxMemory());
+		System.out.println ("Free : " + r.freeMemory());
+		 
+		ja = null;
+		jb = null;
+	
+		r.gc(); 
+		System.out.println ("Free : " + r.freeMemory());		 
+	
+		ja = new People("John", "Doe", 1970);
+		jb = new People("Jane", "Doe", 1980);	 
+		r.gc(); 
+		System.out.println ("Free : " + r.freeMemory());
+	}
+	
+	
+}
+
